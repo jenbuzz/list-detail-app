25a87f9a0b07abb1c392576aacbcf1eb17b98e58
dan-lyn <daniel_lynge@hotmail.com>
2018-05-28 20:29:20 +0200

    move fontawesome declarations to constructor to fix build process

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,10 +24,6 @@ import regular from '@fortawesome/fontawesome-free-regular';
 import brands from '@fortawesome/fontawesome-free-brands';
 import fontawesome from '@fortawesome/fontawesome';
 
-fontawesome.library.add(solid);
-fontawesome.library.add(regular);
-fontawesome.library.add(brands);
-
 export function loadConfig(configService: ConfigService) {
     return () => {
         return configService.load();
@@ -59,4 +55,10 @@ export function loadConfig(configService: ConfigService) {
     ],
     bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+    constructor() {
+        fontawesome.library.add(solid);
+        fontawesome.library.add(regular);
+        fontawesome.library.add(brands);
+    }
+}
