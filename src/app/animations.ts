12900b98b2e7255bd606c94978b11d13a3396d8c
dan-lyn <daniel_lynge@hotmail.com>
2018-05-29 21:16:50 +0200

    updated route animation to fix absolute position problem

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,30 +1,26 @@
 import { trigger, animate, transition, style, group, query } from '@angular/animations';
 
 export const routeAnimation = trigger('routeAnimation', [
-    transition('* <=> *', [
-        query(':enter',
-            style({
-                opacity: 0
-            }),
-            {optional: true}
-        ),
+    transition('list => detail', [
+        style({ height: '!' }),
+        query(':enter', style({ opacity: 0 })),
+        query(':enter, :leave', style({ position: 'absolute', top: 0, left: 0, right: 0 })),
         group([
-            query(':leave',
-                animate('.5s ease',
-                    style({
-                        opacity: 0
-                    })
-                ),
-                {optional: true}
-            ),
-            query(':enter',
-                animate('.5s ease',
-                    style({
-                    opacity: 1
-                    })
-                ),
-                {optional: true}
-            )
-        ])
+            query(':leave', [
+                animate('.5s', style({ opacity: 0 })),
+            ]),
+            query(':enter', animate('.5s', style({ opacity: 1 }))),
+        ]),
+    ]),
+    transition('detail => list', [
+        style({ height: '!' }),
+        query(':enter', style({  opacity: 0 })),
+        query(':enter, :leave', style({ position: 'absolute', top: 0, left: 0, right: 0 })),
+        group([
+            query(':leave', [
+                animate('.5s', style({ opacity: 0 })),
+            ]),
+            query(':enter', animate('.5s', style({ opacity: 1 }))),
+        ]),
     ])
 ]);
