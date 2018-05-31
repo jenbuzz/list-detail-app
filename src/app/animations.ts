3d95c98122112e4060704a21383be9320d2e1147
dan-lyn <daniel_lynge@hotmail.com>
2018-05-31 09:05:08 +0200

    fixed animation bug in detail view

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,26 +1,34 @@
 import { trigger, animate, transition, style, group, query } from '@angular/animations';
 
 export const routeAnimation = trigger('routeAnimation', [
-    transition('list => detail', [
+    transition('* => *', [
         style({ height: '!' }),
-        query(':enter', style({ opacity: 0 })),
-        query(':enter, :leave', style({ position: 'absolute', top: 0, left: 0, right: 0 })),
+        query(':enter', style({
+            opacity: 0
+        }), {
+            optional: true
+        }),
+        query(':enter, :leave', style({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0
+        }), {
+            optional: true
+        }),
         group([
             query(':leave', [
-                animate('.5s', style({ opacity: 0 })),
-            ]),
-            query(':enter', animate('.5s', style({ opacity: 1 }))),
-        ]),
-    ]),
-    transition('detail => list', [
-        style({ height: '!' }),
-        query(':enter', style({  opacity: 0 })),
-        query(':enter, :leave', style({ position: 'absolute', top: 0, left: 0, right: 0 })),
-        group([
-            query(':leave', [
-                animate('.5s', style({ opacity: 0 })),
-            ]),
-            query(':enter', animate('.5s', style({ opacity: 1 }))),
+                animate('.5s', style({
+                    opacity: 0
+                })),
+            ], {
+                optional: true
+            }),
+            query(':enter', animate('.5s', style({
+                opacity: 1
+            })), {
+                optional: true
+            }),
         ]),
     ])
 ]);
