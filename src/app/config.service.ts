6f77a0455aab329a808b7f901f2f8e8dde1c9d77
dan-lyn <daniel_lynge@hotmail.com>
2018-08-31 20:42:21 +0200

    fixed assets directory for config files, now forced to be in root dir

--- src/app/config.service.ts
+++ src/app/config.service.ts
@@ -24,7 +24,7 @@ export class ConfigService {
 
     load(): Promise<any> {
         return new Promise((resolve, reject) => {
-            this.http.get('assets/' + this.getConfigFilename()).subscribe( (config: Config) => {
+            this.http.get('/assets/' + this.getConfigFilename()).subscribe( (config: Config) => {
                 this.config = config;
                 resolve(true);
             });
