961803a6b3c1c180af64b0d5b343be2df3ab4f67
jenbuzz <daniel_lynge@hotmail.com>
2020-04-17 12:38:15 +0200

    create concat build file for es2015

--- scripts/concat-dist.js
+++ scripts/concat-dist.js
@@ -6,3 +6,9 @@ concat([
     path.join(__dirname, '..', 'dist', 'polyfills-es5.js'),
     path.join(__dirname, '..', 'dist', 'main-es5.js'),
 ], path.join(__dirname, '..', 'dist', 'bundle.js'));
+
+concat([
+    path.join(__dirname, '..', 'dist', 'runtime-es2015.js'),
+    path.join(__dirname, '..', 'dist', 'polyfills-es2015.js'),
+    path.join(__dirname, '..', 'dist', 'main-es2015.js'),
+], path.join(__dirname, '..', 'dist', 'bundle-es2015.js'));
